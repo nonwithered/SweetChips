f6b15b5bac1992b9e52e2ff1ea3c20d4546c73d1
nonwithered <1023001754@qq.com>
2021-01-15 01:42:46 +0800

    feature: plugin4gradle MultiTransform

---
+++ plugin4gradle/src/main/java/org/sweetchips/plugin4gradle/BaseContext.java
@@ -0,0 +1,49 @@
+package org.sweetchips.plugin4gradle;
+
+import org.objectweb.asm.ClassVisitor;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.Consumer;
+
+public class UnionContext {
+
+    private final String mName;
+
+    private final BaseExtension mExt;
+
+    private final Collection<Class<? extends ClassVisitor>> mPrepare;
+
+    private final Collection<Class<? extends ClassVisitor>> mDump;
+
+    public UnionContext(String name, BaseExtension ext) {
+        mName = name;
+        mExt = ext;
+        mPrepare = new ConcurrentLinkedQueue<>();
+        mDump = new ConcurrentLinkedQueue<>();
+    }
+
+    String getName() {
+        return mName;
+    }
+
+    BaseExtension getExtension() {
+        return mExt;
+    }
+
+    public void addPrepare(Class<? extends ClassVisitor> clazz) {
+        mPrepare.add(clazz);
+    }
+
+    public void addDump(Class<? extends ClassVisitor> clazz) {
+        mDump.add(clazz);
+    }
+
+    void forEachPrepare(Consumer<Class<? extends ClassVisitor>> consumer) {
+        mPrepare.forEach(consumer);
+    }
+
+    void forEachDump(Consumer<Class<? extends ClassVisitor>> consumer) {
+        mDump.forEach(consumer);
+    }
+}
