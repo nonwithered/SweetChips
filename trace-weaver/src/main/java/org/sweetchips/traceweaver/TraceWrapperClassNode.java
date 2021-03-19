cf59ed9f42d4816904b81ad5d85cddfe81598f9c
nonwithered <1023001754@qq.com>
2021-03-19 22:36:22 +0800

    feature: compile util

--- trace-weaver/src/main/java/org/sweetchips/traceweaver/TraceWrapperClassNode.java
+++ trace-weaver/src/main/java/org/sweetchips/traceweaver/TraceWrapperClassNode.java
@@ -1,42 +1,20 @@
 package org.sweetchips.traceweaver;
 
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Scanner;
-
-import javax.tools.FileObject;
-import javax.tools.ForwardingJavaFileManager;
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileManager;
-import javax.tools.JavaFileObject;
-import javax.tools.SimpleJavaFileObject;
-import javax.tools.ToolProvider;
+import org.sweetchips.utility.ClassesUtil;
+import org.sweetchips.utility.FilesUtil;
 
 final class TraceWrapperClassNode extends ClassNode {
 
     TraceWrapperClassNode(int api) {
         super(api);
-        init();
-    }
-
-    private void init() {
-        try {
-            ClassReader cr = new ClassReader(getTraceWrapper());
-            cr.accept(this, ClassReader.EXPAND_FRAMES);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        new ClassReader(ClassesUtil.compile(Util.TRACE_WRAPPER_CLASS_NAME,
+                () -> new String(FilesUtil.readFrom(getClass().getResourceAsStream(Util.TRACE_WRAPPER_SOURCE))),
+                System.err::println)).accept(this, ClassReader.EXPAND_FRAMES);
     }
 
     @Override
@@ -68,91 +46,5 @@ final class TraceWrapperClassNode extends ClassNode {
                 super.visitMethodInsn(opcode, owner, name, desc, itf);
             }
         };
-    }
-
-    private byte[] getTraceWrapper() throws IOException {
-        try (InputStream input = getClass().getResourceAsStream(Util.TRACE_WRAPPER_SOURCE);
-             ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            JavaFileObjectImpl javaFileObject =
-                    new JavaFileObjectImpl(input, output);
-            JavaFileManagerImpl javaFileManager =
-                    new JavaFileManagerImpl(compiler.getStandardFileManager(null, null, null),
-                            javaFileObject);
-            boolean b = compiler.getTask(null,
-                    javaFileManager,
-                    null,
-                    null,
-                    null,
-                    Collections.singletonList(javaFileObject)).call();
-            if (!b) {
-                throw new IOException();
-            }
-            return output.toByteArray();
-        }
-    }
-
-    private static final class JavaFileManagerImpl extends ForwardingJavaFileManager<JavaFileManager> {
-
-        private final JavaFileObject mFileObject;
-
-        private JavaFileManagerImpl(JavaFileManager fileManager, JavaFileObject fileObject) {
-            super(fileManager);
-            mFileObject = fileObject;
-        }
-
-        @Override
-        public JavaFileObject getJavaFileForInput(Location location, String className, JavaFileObject.Kind kind) {
-            return mFileObject;
-        }
-
-        @Override
-        public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) {
-            return mFileObject;
-        }
-    }
-
-    private static final class JavaFileObjectImpl extends SimpleJavaFileObject {
-
-        private final InputStream mInput;
-        private final OutputStream mOutput;
-
-        private JavaFileObjectImpl(InputStream input, OutputStream output) {
-            super(TraceWeaverPlugin.INSTANCE.getProject()
-                            .getRootDir().toPath()
-                            .resolve("build")
-                            .resolve(Paths.get("/")
-                                    .relativize(Paths.get(Util.TRACE_WRAPPER_SOURCE))).toUri(),
-                    Kind.SOURCE);
-            mInput = input;
-            mOutput = output;
-        }
-
-        @Override
-        public InputStream openInputStream() {
-            return mInput;
-        }
-
-        @Override
-        public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
-            return getContent(openInputStream());
-        }
-
-        @Override
-        public OutputStream openOutputStream() {
-            return mOutput;
-        }
-
-        private static String getContent(InputStream inputStream) throws IOException {
-            StringBuilder stringBuilder = new StringBuilder();
-            try (InputStream input = inputStream;
-                 Scanner scanner = new Scanner(inputStream)
-            ) {
-                while (scanner.hasNextLine()) {
-                    stringBuilder.append(scanner.nextLine());
-                }
-            }
-            return stringBuilder.toString();
-        }
     }
 }
--- utility/src/main/java/org/sweetchips/utility/ClassesUtil.java
+++ utility/src/main/java/org/sweetchips/utility/ClassesUtil.java
@@ -1,6 +1,22 @@
 package org.sweetchips.utility;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.Constructor;
+import java.net.URI;
+import java.util.Collections;
+import java.util.function.Supplier;
+
+import javax.tools.DiagnosticCollector;
+import javax.tools.DiagnosticListener;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 
 public interface ClassesUtil {
 
@@ -15,5 +31,44 @@ public interface ClassesUtil {
 
     static <T> T newInstance(Constructor<T> constructor, Object... args) {
         return AsyncUtil.call(() ->  constructor.newInstance(args));
+    }
+
+    static byte[] compile(String name, Supplier<String> content, DiagnosticListener<JavaFileObject> listener) {
+        String str = name.replaceAll("\\.","/");
+        return AsyncUtil.call(() -> {
+            try (ByteArrayOutputStream bytes = new ByteArrayOutputStream()) {
+                JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+                JavaFileObject source = new SimpleJavaFileObject(URI.create("string:///" + str + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE) {
+                    @Override
+                    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+                        return content.get();
+                    }
+                };
+                JavaFileObject target = new SimpleJavaFileObject(URI.create("string:///" + str + JavaFileObject.Kind.CLASS.extension), JavaFileObject.Kind.CLASS) {
+                    @Override
+                    public OutputStream openOutputStream() {
+                        return bytes;
+                    }
+                };
+                JavaFileManager manager = new ForwardingJavaFileManager<JavaFileManager>(compiler.getStandardFileManager(null, null, null)) {
+                    @Override
+                    public JavaFileObject getJavaFileForInput(Location location, String className, JavaFileObject.Kind kind) {
+                        return source;
+                    }
+                    @Override
+                    public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) {
+                        return target;
+                    }
+                };
+                DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<>();
+                if (!compiler.getTask(null, manager, collector, null, null, Collections.singletonList(source)).call()) {
+                    if (listener != null) {
+                        collector.getDiagnostics().forEach(listener::report);
+                    }
+                    throw new IOException();
+                }
+                return bytes.toByteArray();
+            }
+        });
     }
 }
